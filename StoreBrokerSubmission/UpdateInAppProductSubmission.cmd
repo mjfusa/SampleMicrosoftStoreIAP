45b0382abce4c790891305fba1a293a2e66f9128
Mike Francis <mike.francis@live.com>
2021-12-21 17:23:44 -0800

    Durable and Consumable Listings

---
+++ StoreBrokerSubmission/UpdateInAppProductSubmission.cmd
@@ -0,0 +1,2 @@
+
+PowerShell Update-InAppProductSubmission -IapId %1 -SubmissionDataPath .\Durables\%2\Submisson\Submission.json -PackagePath .\Durables\%2\Submisson\Submission.zip -Force -UpdateListings
